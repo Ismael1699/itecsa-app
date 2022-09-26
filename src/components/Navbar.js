09c90f47d8f2bcc1dae2352556e1ce7398eda341
Xisma <ismaelrodolfo15@gmail.com>
2022-09-26 17:25:19 -0500

    Se termina de crear el componente Navbar y se comienza con el componente Search

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import '../styles/Navbar.css';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav id='navbar'>
+                <ul id='nav-container'>
+                    <div className='item-container'>
+                        <i className='bi bi-house-fill'></i>
+                        <li>Home</li>
+                    </div>
+                    <div className='item-container'>
+                        <i className='bi bi-grid-1x2-fill'></i>
+                        <li>DashBoard</li>
+                    </div>
+                    <div className='item-container'>
+                        <i className='bi bi-book-fill'></i>
+                        <li>Estimaciones</li>
+                    </div>
+                    <div className='item-container'>
+                        <i className='bi bi-symmetry-vertical'></i>
+                        <li>Larguillo avance</li>
+                    </div>
+                    <div className='item-container'>
+                        <i className='bi bi-fuel-pump-fill'></i>
+                        <li>Cargas diésel</li>
+                    </div>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
