62eeba39aa344f3fcbdfa3eaf838d6a8095ec524
Xisma <ismaelrodolfo15@gmail.com>
2022-09-27 13:22:10 -0500

    Se termino de corregir un bug con el body y se mejoro el leyer app ahora es mas responsive

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Content from './components/Content';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
+import './App.css';
 
 function App() {
     return (
-        <div>
-            <Navbar />
-            <Search />
-            <Content />
+        <div id='app-container'>
+            <div id='item-app-navbar'></div>
+            <div id='item-app-content'></div>
         </div>
     );
 }
