92aa84bb9f8fc95d28ff527708f6b045718ea2fc
Xisma <ismaelrodolfo15@gmail.com>
2022-09-27 19:20:00 -0500

    Se cambio el archivo Readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import './App.css';
 function App() {
     return (
         <div id='app-container'>
-            <div id='item-app-navbar'></div>
+            <div id='item-app-navbar'>
+                <Navbar />
+            </div>
             <div id='item-app-content'></div>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,32 +3,28 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <>
-            <nav id='navbar'>
-                <ul id='nav-container'>
-                    <div className='item-container'>
-                        <i className='bi bi-house-fill'></i>
-                        <li>Home</li>
-                    </div>
-                    <div className='item-container'>
-                        <i className='bi bi-grid-1x2-fill'></i>
-                        <li>DashBoard</li>
-                    </div>
-                    <div className='item-container'>
-                        <i className='bi bi-symmetry-vertical'></i>
-                        <li>Avance Diario</li>
-                    </div>
-                    <div className='item-container'>
-                        <i className='bi bi-book-fill'></i>
-                        <li>Estimaciones</li>
-                    </div>
-                    <div className='item-container'>
-                        <i className='bi bi-fuel-pump-fill'></i>
-                        <li>Cargas diésel</li>
-                    </div>
-                </ul>
-            </nav>
-        </>
+        <div id='nav-container'>
+            <div className='item-container'>
+                <i className='bi bi-house-fill'></i>
+                <h1>Home</h1>
+            </div>
+            <div className='item-container'>
+                <i className='bi bi-grid-1x2-fill'></i>
+                <h1>DashBoard</h1>
+            </div>
+            <div className='item-container'>
+                <i className='bi bi-symmetry-vertical'></i>
+                <h1>Avance Diario</h1>
+            </div>
+            <div className='item-container'>
+                <i className='bi bi-book-fill'></i>
+                <h1>Estimaciones</h1>
+            </div>
+            <div className='item-container'>
+                <i className='bi bi-fuel-pump-fill'></i>
+                <h1>Cargas diésel</h1>
+            </div>
+        </div>
     );
 };
 
