9665d59cadc2d7d661ef9e0b670934d9dd525fcd
Xisma <ismaelrodolfo15@gmail.com>
2022-09-29 16:20:23 -0500

    se hace cambio de id de los elementos dentro del componente NavWeb.js asi como tambien en NavWeb.css

--- src/components/NavWeb.js
+++ src/components/NavWeb.js
@@ -3,24 +3,24 @@ import '../styles/NavWeb.css';
 
 const NavWeb = () => {
     return (
-        <div id='navWeb-container'>
-            <div className='item-container-web'>
+        <div id='navWeb'>
+            <div className='item-web'>
                 <i className='bi bi-house-fill'></i>
                 <h1>Home</h1>
             </div>
-            <div className='item-container-web'>
+            <div className='item-web'>
                 <i className='bi bi-grid-1x2-fill'></i>
                 <h1>DashBoard</h1>
             </div>
-            <div className='item-container-web'>
+            <div className='item-web'>
                 <i className='bi bi-symmetry-vertical'></i>
                 <h1>Avance Diario</h1>
             </div>
-            <div className='item-container-web'>
+            <div className='item-web'>
                 <i className='bi bi-book-fill'></i>
                 <h1>Estimaciones</h1>
             </div>
-            <div className='item-container-web'>
+            <div className='item-web'>
                 <i className='bi bi-fuel-pump-fill'></i>
                 <h1>Cargas diésel</h1>
             </div>
