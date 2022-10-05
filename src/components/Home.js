7af358657652a99eae544a30f000baaf6434a9cb
Xisma <ismaelrodolfo15@gmail.com>
2022-10-05 12:01:50 -0500

    se arreglo el problema de que los elementos de section de app se salian fuera del tamaño de pantalla se arreglo con flex

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const ContainerHome = styled.div`
+	height: 100%;
+	border-radius: 30px;
+`;
 
 const Home = () => {
-    return (
-        <>
-            <h1>
-                ¡Bienvendio al software de control y administración de
-                Triturados basalticos de Tepetlaoxtoc!
-            </h1>
-        </>
-    );
+	return (
+		<ContainerHome>
+			<h1>
+				¡Bienvendio al software de control y administración de
+				Triturados basalticos de Tepetlaoxtoc!
+			</h1>
+		</ContainerHome>
+	);
 };
 
 export default Home;
