6bf7916bf389b544be89979e35bb612c893fdee8
Xisma <ismaelrodolfo15@gmail.com>
2022-10-07 12:53:32 -0500

    se implementa img al navbarMobile, corregi parametros responsive

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuMobile from './MenuMobile';
 import '../styles/NavMobile.css';
 import { Component, useState } from 'react';
+import user from '../img/user.jpeg';
 
 const NavMobile = ({
 	homePage,
@@ -29,7 +30,10 @@ const NavMobile = ({
 				toggleMenuButton={toggleMenuButton}
 			/>
 			<p>{loadPage}</p>
-			<i id='user-icon' className='bi bi-person-circle'></i>
+			<div className='img-navmobile'>
+				<img src={user}></img>
+			</div>
+			{/* <i id='user-icon' className='bi bi-person-circle'></i> */}
 		</div>
 	);
 };
