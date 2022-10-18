18796e986ed10d3219898fc7b5170b8d7b6363a8
xisma16 <ismaelrodolfo15@gmail.com>
2022-10-18 18:17:01 -0500

    se cambiaron variables de color a los elementos generados, se agrearon transition a esto items y agrego smooth al scroll de avance

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,102 +1,102 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const ContainerDash = styled.div`
-	width: 100%;
-	height: 100%;
-	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-	gap: 18px 18px;
-	grid-template-areas:
-		'home nominas estimaciones estimaciones'
-		'home nominas estimaciones estimaciones'
-		'compras otros estimaciones estimaciones'
-		'compras otros estimaciones estimaciones'
-		'avances avances estimaciones estimaciones'
-		'avances avances estimaciones estimaciones'
-		'avances avances maquinaria almacen'
-		'diesel diesel maquinaria almacen'
-		'diesel diesel maquinaria almacen';
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    gap: 18px 18px;
+    grid-template-areas:
+        "home nominas estimaciones estimaciones"
+        "home nominas estimaciones estimaciones"
+        "compras otros estimaciones estimaciones"
+        "compras otros estimaciones estimaciones"
+        "avances avances estimaciones estimaciones"
+        "avances avances estimaciones estimaciones"
+        "avances avances maquinaria almacen"
+        "diesel diesel maquinaria almacen"
+        "diesel diesel maquinaria almacen";
 
-	#dash-home {
-		grid-area: home;
-	}
+    #dash-home {
+        grid-area: home;
+    }
 
-	#dash-avance {
-		grid-area: avances;
-	}
+    #dash-avance {
+        grid-area: avances;
+    }
 
-	#dash-estimaciones {
-		grid-area: estimaciones;
-	}
+    #dash-estimaciones {
+        grid-area: estimaciones;
+    }
 
-	#dash-diesel {
-		grid-area: diesel;
-	}
+    #dash-diesel {
+        grid-area: diesel;
+    }
 
-	#dash-maquinaria {
-		grid-area: maquinaria;
-	}
+    #dash-maquinaria {
+        grid-area: maquinaria;
+    }
 
-	#dash-compras {
-		grid-area: compras;
-	}
+    #dash-compras {
+        grid-area: compras;
+    }
 
-	#dash-nominas {
-		grid-area: nominas;
-	}
+    #dash-nominas {
+        grid-area: nominas;
+    }
 
-	#dash-almacen {
-		grid-area: almacen;
-	}
+    #dash-almacen {
+        grid-area: almacen;
+    }
 
-	#dash-otros {
-		grid-area: otros;
-	}
+    #dash-otros {
+        grid-area: otros;
+    }
 
-	.dash-item {
-		background-color: #e2eb94;
-		border-radius: 18px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		color: #675900;
-	}
+    .dash-item {
+        background-color: var(--third-color);
+        border-radius: 18px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+    }
 `;
 
 const DashBoard = () => {
-	return (
-		<ContainerDash>
-			<div id='dash-home' className='dash-item'>
-				<h2>home</h2>
-			</div>
-			<div id='dash-avance' className='dash-item'>
-				<h2>Avance</h2>
-			</div>
-			<div id='dash-estimaciones' className='dash-item'>
-				<h2>Estimaciones</h2>
-			</div>
-			<div id='dash-diesel' className='dash-item'>
-				<h2>Diésel</h2>
-			</div>
-			<div id='dash-maquinaria' className='dash-item'>
-				<h2>Maquinaria</h2>
-			</div>
-			<div id='dash-compras' className='dash-item'>
-				<h2>Compras</h2>
-			</div>
-			<div id='dash-nominas' className='dash-item'>
-				<h2>Nominas</h2>
-			</div>
-			<div id='dash-almacen' className='dash-item'>
-				<h2>Almacen</h2>
-			</div>
-			<div id='dash-otros' className='dash-item'>
-				<h2>Otros</h2>
-			</div>
-		</ContainerDash>
-	);
+    return (
+        <ContainerDash>
+            <div id='dash-home' className='dash-item'>
+                <h2>home</h2>
+            </div>
+            <div id='dash-avance' className='dash-item'>
+                <h2>Avance</h2>
+            </div>
+            <div id='dash-estimaciones' className='dash-item'>
+                <h2>Estimaciones</h2>
+            </div>
+            <div id='dash-diesel' className='dash-item'>
+                <h2>Diésel</h2>
+            </div>
+            <div id='dash-maquinaria' className='dash-item'>
+                <h2>Maquinaria</h2>
+            </div>
+            <div id='dash-compras' className='dash-item'>
+                <h2>Compras</h2>
+            </div>
+            <div id='dash-nominas' className='dash-item'>
+                <h2>Nominas</h2>
+            </div>
+            <div id='dash-almacen' className='dash-item'>
+                <h2>Almacen</h2>
+            </div>
+            <div id='dash-otros' className='dash-item'>
+                <h2>Otros</h2>
+            </div>
+        </ContainerDash>
+    );
 };
 
 export default DashBoard;
