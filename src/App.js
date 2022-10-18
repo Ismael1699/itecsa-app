339a844b6f37fb32e684489d58a3f66f8f79cf88
xisma16 <ismaelrodolfo15@gmail.com>
2022-10-18 12:57:48 -0500

    se agrego carpetas para organizar mejor los componentes

--- src/App.js
+++ src/App.js
@@ -1,111 +1,111 @@
-import React, { useEffect, useState } from 'react';
-import Content from './components/Content';
-import Nav from './components/Nav';
-import './App.css';
-import TopBarWeb from './components/TopBarWeb';
+import React, { useEffect, useState } from "react";
+import Content from "./components/Content";
+import Nav from "./components/nav/Nav";
+import "./App.css";
+import TopBarWeb from "./components/topbar/TopBarWeb";
 
 function App() {
-	const [displayWidth, setDisplayWidth] = useState(window.innerWidth);
-	const [loadPage, setLoadPage] = useState('home');
-	const [areaNamesData, setAreaNamesData] = useState([
-		'home',
-		'hola',
-		'hora',
-		'hay',
-		'dashboard',
-		'day',
-		'daily',
-		'derevil',
-		'dos',
-		'avance',
-		'abra',
-		'avanlo',
-		'avion',
-		'avioacion',
-		'estimaciones',
-		'eso',
-		'esos',
-		'esontato',
-		'diesel',
-		'dia',
-		'dialorgar',
-		'diatono',
-	]);
+    const [displayWidth, setDisplayWidth] = useState(window.innerWidth);
+    const [loadPage, setLoadPage] = useState("home");
+    const [areaNamesData, setAreaNamesData] = useState([
+        "home",
+        "hola",
+        "hora",
+        "hay",
+        "dashboard",
+        "day",
+        "daily",
+        "derevil",
+        "dos",
+        "avance",
+        "abra",
+        "avanlo",
+        "avion",
+        "avioacion",
+        "estimaciones",
+        "eso",
+        "esos",
+        "esontato",
+        "diesel",
+        "dia",
+        "dialorgar",
+        "diatono",
+    ]);
 
-	const getDisplaySize = () => {
-		setDisplayWidth(window.innerWidth);
-	};
+    const getDisplaySize = () => {
+        setDisplayWidth(window.innerWidth);
+    };
 
-	const homePage = () => {
-		setLoadPage('home');
-	};
+    const homePage = () => {
+        setLoadPage("home");
+    };
 
-	const dashBoardPage = () => {
-		setLoadPage('dashboard');
-	};
+    const dashBoardPage = () => {
+        setLoadPage("dashboard");
+    };
 
-	const avancePage = () => {
-		setLoadPage('avance');
-	};
+    const avancePage = () => {
+        setLoadPage("avance");
+    };
 
-	const estimacionesPage = () => {
-		setLoadPage('estimaciones');
-	};
+    const estimacionesPage = () => {
+        setLoadPage("estimaciones");
+    };
 
-	const dieselPage = () => {
-		setLoadPage('diesel');
-	};
+    const dieselPage = () => {
+        setLoadPage("diesel");
+    };
 
-	const handleSearch = (text) => {
-		switch (text) {
-			case 'home':
-				homePage();
-				break;
-			case 'dashboard':
-				dashBoardPage();
-				break;
-			case 'avance':
-				avancePage();
-				break;
-			case 'estimaciones':
-				estimacionesPage();
-				break;
-			case 'diesel':
-				dieselPage();
-				break;
-		}
-	};
+    const handleSearch = (text) => {
+        switch (text) {
+            case "home":
+                homePage();
+                break;
+            case "dashboard":
+                dashBoardPage();
+                break;
+            case "avance":
+                avancePage();
+                break;
+            case "estimaciones":
+                estimacionesPage();
+                break;
+            case "diesel":
+                dieselPage();
+                break;
+        }
+    };
 
-	//Con este useEffect se obtine el ancho de la pantalla en todo el tiempo de ejecución del proyecto
-	useEffect(() => {
-		window.addEventListener('resize', getDisplaySize);
-	});
-	return (
-		<div id='app'>
-			<div id='container-nav'>
-				<Nav
-					displayWidth={displayWidth}
-					homePage={homePage}
-					dashBoardPage={dashBoardPage}
-					avancePage={avancePage}
-					estimacionesPage={estimacionesPage}
-					dieselPage={dieselPage}
-					loadPage={loadPage}
-				/>
-			</div>
-			<div id='container-section'>
-				{displayWidth > 600 ? (
-					<TopBarWeb
-						areaNamesData={areaNamesData}
-						handleSearch={handleSearch}
-					/>
-				) : (
-					<></>
-				)}
-				<Content loadPage={loadPage} />
-			</div>
-		</div>
-	);
+    //Con este useEffect se obtine el ancho de la pantalla en todo el tiempo de ejecución del proyecto
+    useEffect(() => {
+        window.addEventListener("resize", getDisplaySize);
+    });
+    return (
+        <div id='app'>
+            <div id='container-nav'>
+                <Nav
+                    displayWidth={displayWidth}
+                    homePage={homePage}
+                    dashBoardPage={dashBoardPage}
+                    avancePage={avancePage}
+                    estimacionesPage={estimacionesPage}
+                    dieselPage={dieselPage}
+                    loadPage={loadPage}
+                />
+            </div>
+            <div id='container-section'>
+                {displayWidth > 600 ? (
+                    <TopBarWeb
+                        areaNamesData={areaNamesData}
+                        handleSearch={handleSearch}
+                    />
+                ) : (
+                    <></>
+                )}
+                <Content loadPage={loadPage} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import '../styles/Content.css';
-import Home from './Home.js';
-import DashBoard from './DashBoard.js';
-import Avance from './Avance.js';
-import Estimaciones from './Estimaciones.js';
-import Diesel from './Diesel.js';
+import React from "react";
+import "../styles/Content.css";
+import Home from "./Home.js";
+import DashBoard from "./DashBoard.js";
+import Avance from "./avance/Avance.js";
+import Estimaciones from "./Estimaciones.js";
+import Diesel from "./Diesel.js";
 
 const Content = ({ loadPage }) => {
-	return (
-		<div id='content-container'>
-			{
-				{
-					home: <Home />,
-					dashboard: <DashBoard />,
-					avance: <Avance />,
-					estimaciones: <Estimaciones />,
-					diesel: <Diesel />,
-				}[loadPage]
-			}
-		</div>
-	);
+    return (
+        <div id='content-container'>
+            {
+                {
+                    home: <Home />,
+                    dashboard: <DashBoard />,
+                    avance: <Avance />,
+                    estimaciones: <Estimaciones />,
+                    diesel: <Diesel />,
+                }[loadPage]
+            }
+        </div>
+    );
 };
 
 export default Content;
