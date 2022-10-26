f1eb851ae0789d3c640fd2c5d5e63596a4cd6345
xisma16 <ismaelrodolfo15@gmail.com>
2022-10-26 11:48:22 -0500

    se agrega mas datos al archivo conceptos  json para poder generar interfaz de manera mas eficiente

--- src/components/avance/Conceptos.js
+++ src/components/avance/Conceptos.js
@@ -5,7 +5,25 @@ const Conceptos = ({ handleClick }) => {
     return (
         <div className='all-conceptos'>
             <div id='fresado' className='item-avance' onClick={handleClick}>
-                <p>Fresado</p>
+                <div className='concept'>
+                    <h2>Fresado</h2>
+                </div>
+                <div className='avance'>
+                    <p>Avance</p>
+                </div>
+                <div className='actual'>
+                    <p>Actual</p>
+                </div>
+                <div className='meta'>
+                    <p>Meta</p>
+                </div>
+                <div className='barraAvance'></div>
+                <div className='actualNumber'></div>
+                <div className='metaNumber'></div>
+                <div className='subconcepto1'></div>
+                <div className='cirBar1'></div>
+                <div className='price1'></div>
+                <div className='price1'></div>
             </div>
             <div className='item-avance'></div>
             <div className='item-avance'></div>
