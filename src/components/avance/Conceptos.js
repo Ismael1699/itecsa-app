1fa05a10abb889392513055375c8767d6445214e
xisma16 <ismaelrodolfo15@gmail.com>
2022-10-27 13:33:07 -0500

    ya se visualiza los datos generales del concepto en su respectivo card, ahora se implementara sus estilos correspondientes

--- src/components/avance/Conceptos.js
+++ src/components/avance/Conceptos.js
@@ -12,7 +12,15 @@ const Conceptos = ({ handleClick }) => {
                 key={keys}
                 className='item-avance'
                 onClick={handleClick}>
-                {keys}
+                <div className='concept'>{keys}</div>
+                <div className='avance'>Avance</div>
+                <div className='actual'>Actual</div>
+                <div className='meta'>Meta</div>
+                <div className='barraAvance'>
+                    barra {data[keys].general.percentaje}
+                </div>
+                <div className='actualNumber'>{data[keys].general.avance}</div>
+                <div className='metaNumber'>{data[keys].general.meta}</div>
             </div>
         );
     }
