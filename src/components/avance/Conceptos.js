6d1d69c4165e33a21280b451cff7c1cd98b8749a
xisma16 <ismaelrodolfo15@gmail.com>
2022-10-28 12:08:21 -0500

    se agrego un barra de porcentaje, a los conceptos generados y se cambio el grid de cada uno para adaptarse mejor al tamaño de pantalla

--- src/components/avance/Conceptos.js
+++ src/components/avance/Conceptos.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "../../styles/Conceptos.css";
 import data from "../../data/concepts.json";
+import styled from "styled-components";
+
+const BarProgressGeneral = styled.div`
+    width: 60%;
+    height: 30%;
+    background-color: #6b554a;
+    overflow: hidden;
+    border-radius: 10px;
+    &::before {
+        content: "";
+        display: block;
+        width: ${(props) => "" + props.percentaje}%;
+        height: 100%;
+        background-color: #2fff24;
+        z-index: 3;
+        border-radius: 10px;
+    }
+`;
 
 const Conceptos = ({ handleClick }) => {
     // el siguiente bucle y array me permiten generar elementos jsx de forma automatica segun el tamaño de los datos
     let arrConcepts = [];
+    const moneda = Intl.NumberFormat("es-MX", {
+        style: "currency",
+        currency: "MXN",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
     for (let keys in data) {
         arrConcepts.push(
             <div
@@ -13,14 +37,19 @@ const Conceptos = ({ handleClick }) => {
                 className='item-avance'
                 onClick={handleClick}>
                 <div className='concept'>{keys}</div>
-                <div className='avance'>Avance</div>
                 <div className='actual'>Actual</div>
                 <div className='metaGeneral'>Meta</div>
                 <div className='barraAvance'>
-                    barra {data[keys].general.percentaje}
+                    <BarProgressGeneral
+                        percentaje={data[keys].general.percentaje}
+                    />
                 </div>
-                <div className='actualNumber'>{data[keys].general.avance}</div>
-                <div className='metaNumber'>{data[keys].general.meta}</div>
+                <div className='actualNumber'>
+                    {moneda.format(data[keys].general.avance)}
+                </div>
+                <div className='metaNumber'>
+                    {moneda.format(data[keys].general.meta)}
+                </div>
                 <div className='proximos'>proximos</div>
             </div>
         );
